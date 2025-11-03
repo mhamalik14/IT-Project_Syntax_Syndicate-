f5beaf47d496811928db668efc8e5e9aa70aa8a0
mhamalik14 <mhamalikhasan@gmail.com>
2025-11-03 17:46:30 +0200

    updated

--- Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container">
-      <h1>Welcome to Que-Quick Reserve</h1>
-      <p>Book and manage clinic appointments with ease.</p>
-    </div>
-  );
-}
---
+++ clinic_scheduler_frontend/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to Que-Quick Reserve</h1>
+      <p>Book and manage clinic appointments with ease.</p>
+    </div>
+  );
+}
